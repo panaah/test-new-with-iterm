040f3d1a436d7c41f7867361d9159d604e9067cb
Thakur VJ <thakurvj@Thakurs-MacBook-Pro.local>
2015-01-06 16:31:08 +0530

    Initial Commit

---
+++ PracticeFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeFirstApp
+//
+//  Created by Thakur VJ on 06/01/15.
+//  Copyright (c) 2015 Thakur VJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
